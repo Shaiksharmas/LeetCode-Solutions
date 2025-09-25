cf37c375bed86fa57f4770833b781f29eb16846f
Shaiksharmas <shaiksharmas024@gmail.com>
2025-09-25 15:48:16 +0530

    Time: 30 ms (70.91%) | Memory: 51.6 MB (56.49%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,35 +1,30 @@
-import java.util.*;
+//optimized sol for the 3sum problem -> O(n^2)
 
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);  
         List<List<Integer>> result = new ArrayList<>();
-        int n = nums.length;
+        Arrays.sort(nums);
 
-        for (int i = 0; i < n - 2; i++) {
-          
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
+        for(int i=0; i<nums.length-2; i++){
+            if(i>0 && nums[i] == nums[i-1]) continue;
+            int left = i+1;
+            int right = nums.length-1;
 
-            int left = i + 1, right = n - 1;
-
-            while (left < right) {
+            while(left<right){ // apply 2pointer approach
                 int sum = nums[i] + nums[left] + nums[right];
 
-                if (sum == 0) {
+                if(sum == 0){
                     result.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                   
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                   
-                    while (left < right && nums[right] == nums[right - 1]) right--;
+
+                    while(left<right && nums[left] == nums[left + 1]) left++;
+                    while(left<right && nums[right] == nums[right - 1]) right--;
 
                     left++;
                     right--;
-                } 
-                else if (sum < 0) {
-                    left++;  
-                } 
-                else {
-                    right--; 
+                }else if(sum<0){
+                    left++;
+                }else{
+                    right--;
                 }
             }
         }
