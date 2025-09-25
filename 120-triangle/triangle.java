db3944f8006e38359ed64d7c03eaa299a204b81b
Shaiksharmas <shaiksharmas024@gmail.com>
2025-09-25 15:49:37 +0530

    Time: 9 ms (11.07%) | Memory: 45.5 MB (42.22%) - LeetSync

---
+++ 120-triangle/triangle.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+    
+        for (int i = triangle.size() - 2; i >= 0; i--) {
+            for (int j = 0; j < triangle.get(i).size(); j++) {
+                int down = triangle.get(i + 1).get(j);
+                int downRight = triangle.get(i + 1).get(j + 1);
+                int curr = triangle.get(i).get(j);
+                
+                triangle.get(i).set(j, curr + Math.min(down, downRight));
+            }
+        }
+        return triangle.get(0).get(0);
+    }
+}
